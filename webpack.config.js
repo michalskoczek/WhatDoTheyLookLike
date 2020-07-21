21985815c7558729b0b7de73ba275ff3bee5e040
Michał Skoczek <mwskoczek@gmail.com>
2020-07-21 17:31:19 +0200

    Added new plugins cleam and html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: 'js/[name]-[contenthash:5].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    open: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
