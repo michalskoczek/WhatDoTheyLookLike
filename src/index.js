a04a6ba859f1c4dd3d40d8ff37af40fee3fc5959
Michał Skoczek <mwskoczek@gmail.com>
2020-07-28 22:23:19 +0200

    Added new module Import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './sass/main.scss';
 import Flickity from './js/Flickity.js';
 import getRandomImages from './js/getDogImg';
+import Image from './js/Image';
 
+const img = new Image;
+img.makeCarouselCell(4, 'dog')
 Flickity();
 getRandomImages('https://dog.ceo/api/breeds/image/random/4');
