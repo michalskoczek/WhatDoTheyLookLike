8ede92b12e4377664f6eca401822d1d102eb9deb
Michał Skoczek <mwskoczek@gmail.com>
2020-07-29 18:07:47 +0200

    Updated class Dog and it methods

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './sass/main.scss';
 import Flickity from './js/Flickity.js';
 import Dog from './js/dog/Dog';
 
+
 const img = new Dog();
-img.makeCarouselCell(4, 'dog');
+img.makeCarouselCell();
 img.putImgToCell();
 Flickity();
