9d975fad8b15de0ac05179db62aa10a88684a554
Michał Skoczek <mwskoczek@gmail.com>
2020-08-31 19:15:09 +0200

    Added new function createBreedElement to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './sass/main.scss';
 import Flickity from './js/Flickity.js';
 import Dog from './js/dog/Dog';
+import createBreedElement from './js/dog/dogsBreeds';
 
 const dog = new Dog();
 dog.makeCarouselCell();
 dog.addCell();
 Flickity();
+createBreedElement(20);
