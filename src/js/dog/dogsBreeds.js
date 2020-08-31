031851fe90f853cb931b4d6516d5f5c891176fee
Michał Skoczek <mwskoczek@gmail.com>
2020-08-31 19:15:40 +0200

    Added new script dogsBreeds.js

---
+++ src/js/dog/dogsBreeds.js
@@ -0,0 +1,14 @@
+function createBreedElement(number) {
+  const breedsContainer = document.querySelector('.breeds');
+  for (let i = 0; i < number; i++) {
+    const breedElement = document.createElement('div');
+    breedElement.className = 'breeds__name';
+    breedsContainer.appendChild(breedElement);
+  }
+}
+
+function getBreedName(url) {
+  // funkcja, która pobiera nazwy ras psów
+}
+
+export default createBreedElement;
