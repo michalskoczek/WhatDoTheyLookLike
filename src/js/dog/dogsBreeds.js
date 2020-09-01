3d610525a4fc8015f8efcce5c93d48457b8bb22e
Michał Skoczek <mwskoczek@gmail.com>
2020-09-01 17:50:20 +0200

    Added import and const number

--- src/js/dog/dogsBreeds.js
+++ src/js/dog/dogsBreeds.js
@@ -1,4 +1,7 @@
-function createBreedElement(number) {
+import loadAllBreedsNames from './getDogData';
+const number = 19;
+
+function createBreedElement() {
   const breedsContainer = document.querySelector('.breeds');
   for (let i = 0; i < number; i++) {
     const breedElement = document.createElement('div');
