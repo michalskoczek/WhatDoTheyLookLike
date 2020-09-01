ff4ddae00728e2a368d2cb73ad3b2b60400985e4
Michał Skoczek <mwskoczek@gmail.com>
2020-09-01 17:17:16 +0200

    Added new function showAllBreeds

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './sass/main.scss';
 import Flickity from './js/Flickity.js';
 import Dog from './js/dog/Dog';
-import createBreedElement from './js/dog/dogsBreeds';
+import createBreedElement, {
+  showAllBreeds
+} from './js/dog/dogsBreeds';
 
 const dog = new Dog();
 dog.makeCarouselCell();
 dog.addCell();
 Flickity();
-createBreedElement(20);
+//createBreedElement(20);
+showAllBreeds();
