9c1cc2926f89ec56718aa95cb8cf03dc60e11f00
Michał Skoczek <mwskoczek@gmail.com>
2020-09-01 17:51:13 +0200

    Updated createBreedElement and add addBreed function

--- src/js/dog/dogsBreeds.js
+++ src/js/dog/dogsBreeds.js
@@ -3,11 +3,27 @@ const number = 19;
 
 function createBreedElement() {
   const breedsContainer = document.querySelector('.breeds');
-  for (let i = 0; i < number; i++) {
-    const breedElement = document.createElement('div');
-    breedElement.className = 'breeds__name';
-    breedsContainer.appendChild(breedElement);
+
+  const breedElement = document.createElement('div');
+  breedElement.className = 'breeds__name';
+  breedElement.innerText = name;
+  breedsContainer.appendChild(breedElement);
+
+}
+
+function addBreed(breed, subBreed) {
+  let name;
+  let type;
+
+  if (typeof subBreed === 'undefined') {
+    name = breed;
+    type = breed;
+  } else {
+    name = `${breed} ${subBreed}`;
+    type = `${breed}/${subBreed}`;
   }
+
+  createBreedElement();
 }
 
 function getBreedName(url) {
