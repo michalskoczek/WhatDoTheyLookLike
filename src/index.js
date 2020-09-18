8d5c6a1b249c550e47a5263cfc66460b19eacfec
Michał Skoczek <mwskoczek@gmail.com>
2020-09-18 19:48:14 +0200

    Added instance of class Cat

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import Dog from './js/dog/Dog';
 import {
   showAllBreeds
 } from './js/dog/dogsBreeds';
+import Cat from './js/cat/Cat';
 
-const dog = new Dog();
+const dog = new Dog('.main-carousel--dog', 4);
 dog.makeCarouselCell();
 dog.addCell();
+const cat = new Cat('.main-carousel--cat', 4);
+cat.makeCarouselCell();
+cat.getAllBreeds();
 Flickity();
 showAllBreeds();
