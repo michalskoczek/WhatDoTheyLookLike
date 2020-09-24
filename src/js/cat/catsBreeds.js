4b2adda3099deb227e28718cc18b19f050677fac
Michał Skoczek <mwskoczek@gmail.com>
2020-09-24 13:55:24 +0200

    Updated handling of the showing images by breed

--- src/js/cat/catsBreeds.js
+++ src/js/cat/catsBreeds.js
@@ -6,49 +6,59 @@ import {
 
 const breedsContainer = document.querySelector('.breeds--cat');
 const loader = document.querySelector('.loader--cat');
-const ArrayOfBreedNames = [];
-const ArrayOfBreedIds = [];
-let breeds = [];
+const ArrayOfBreeds = [];
+const ArrayOfRestBreeds = [];
 const imageURLs = [];
-let nameId = '';
+const apiKey = '12cb4a70-1e20-4fb6-b39f-4aa91e55716b';
 
-function createBreedButtons(name) {
+async function getRandomImagesByBreed(nameId, apiKey) {
+  for (let i = 0; i < 4; i++) {
+    const image = await getCatData(`https://api.thecatapi.com/v1/images/search?breed_ids=${nameId}`, apiKey);
+    imageURLs[i] = await image[0].url;
+  }
+  return imageURLs;
+}
+
+
+function showImageByBreed(urls) {
+  const carouselImgs = [...document.querySelectorAll('.cat-container .carousel-cell__img')];
+  const carouselBackgroundImgs = [...document.querySelectorAll('.cat-container .carousel-cell__background')];
+
+  carouselImgs.forEach((img, index) => {
+    img.src = `${urls[index]}`;
+  });
+  carouselBackgroundImgs.forEach((bgcImg, index) => {
+    bgcImg.style.backgroundImage = `url(${urls[index]})`
+  });
+  hideLoading(loader)
+}
+
+function createBreedButtons(name, id) {
+  const breed = {
+    name: name,
+    id: id,
+  };
+  ArrayOfBreeds.push(breed);
+
   const breedButton = document.createElement('button');
   breedButton.className = 'breeds__name';
   breedButton.innerText = name;
-  breedsContainer.appendChild(breedButton);
 
-  breedButton.addEventListener('click', async (e, apiKey) => {
-    //showLoading(loader);
-    const breedName = e.target.innerText;
-    console.log(breedName);
-    console.log(ArrayOfBreedIds);
-
-    for (const breed of breeds) {
-      if (breed.name === breedName) {
-        nameId = breed.id;
-        return nameId
+  breedButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+    showLoading(loader);
+    for (let i = 0; i < ArrayOfBreeds.length; i++) {
+      if (ArrayOfBreeds[i].name === breedButton.innerText) {
+        const nameId = ArrayOfBreeds[i].id;
+        const urls = await getRandomImagesByBreed(nameId, apiKey);
+        showImageByBreed(urls);
       }
     }
 
-
-    for (let i = 0; i < 4; i++) {
-      const image = await getCatData(`https://api.thecatapi.com/v1/images/search?breed_ids=${nameId}`, apiKey);
-      imageURLs[i] = await image[0].url;
-    }
-
-    const carouselImgs = [...document.querySelectorAll('.cat-container .carousel-cell__img')];
-    const carouselBackgroundImgs = [...document.querySelectorAll('.cat-container .carousel-cell__background')];
-
-    carouselImgs.forEach((img, index) => {
-      img.src = `${imageURLs[index]}`;
-    });
-    carouselBackgroundImgs.forEach((bgcImg, index) => {
-      bgcImg.style.backgroundImage = `url(${imageURLs[index]})`
-    });
-    // hideLoading(loader);
   })
+  breedsContainer.appendChild(breedButton);
 }
+
 
 function createMoreButton() {
   const moreButton = document.createElement('button');
@@ -58,22 +68,25 @@ function createMoreButton() {
 
   moreButton.addEventListener('click', () => {
     moreButton.style.display = 'none';
-    ArrayOfBreedNames.forEach(breed => createBreedButtons(breed))
+    ArrayOfRestBreeds.forEach(breed => createBreedButtons(breed.name, breed.id))
   })
 }
 
+
 async function showAllBreeds(url, apiKey) {
-  breeds = await getCatData(`${url}/breeds`, apiKey);
-  console.log(breeds);
+  const breeds = await getCatData(`${url}/breeds`, apiKey);
 
   breeds.forEach((breed, index) => {
-    ArrayOfBreedIds.push(breed.id);
-    if (index < 19) {
-      createBreedButtons(breed.name);
-    } else if (index === 19) {
+    if (index < 18) {
+      createBreedButtons(breed.name, breed.id);
+    } else if (index === 18) {
+      createBreedButtons(breed.name, breed.id);
       createMoreButton();
-    } else if (index > 19) {
-      ArrayOfBreedNames.push(breed.name);
+    } else {
+      ArrayOfRestBreeds.push({
+        name: breed.name,
+        id: breed.id
+      })
     }
   })
 }
