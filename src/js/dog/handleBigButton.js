589af657756c2db9fa01f1d8a032e4a3a3234125
Michał Skoczek <mwskoczek@gmail.com>
2020-09-25 12:16:17 +0200

    Added handle cat button

--- src/js/dog/handleBigButton.js
+++ src/js/dog/handleBigButton.js
@@ -1,14 +1,17 @@
 function handleBigButton() {
   const dogButton = document.querySelector('.dog-container .pet-button');
+  const catButton = document.querySelector('.cat-container .pet-button');
   const catContainer = document.querySelector('.cat-container');
+  const dogContainer = document.querySelector('.dog-container');
   const catBreeds = document.querySelector('.breeds--cat');
+  const dogBreeds = document.querySelector('.breeds');
 
-  dogButton.addEventListener('click', e => {
+  dogButton.addEventListener('click', (e) => {
     e.preventDefault();
     catContainer.style.display = 'none';
     catBreeds.style.display = 'none';
-    document.querySelector('.breeds').classList.add('breeds--without-cat');
-    document.querySelector('.dog-container').classList.add('dog-container--without-cat');
+    dogBreeds.classList.add('breeds--without-cat');
+    dogContainer.classList.add('dog-container--without-cat');
 
     const arrowButton = document.createElement('button');
     const arrow = document.createElement('i');
@@ -16,26 +19,40 @@ function handleBigButton() {
     arrow.classList.add('fa-arrow-left');
     arrowButton.classList.add('pet-button--arrow');
     arrowButton.appendChild(arrow);
-    document.querySelector('.dog-container').appendChild(arrowButton);
+    dogContainer.appendChild(arrowButton);
 
-    arrowButton.addEventListener('click', e => {
+    arrowButton.addEventListener('click', (e) => {
       e.preventDefault();
       catContainer.style.display = 'block';
       catBreeds.style.display = 'flex';
-      document.querySelector('.breeds').classList.remove('breeds--without-cat');
-      document.querySelector('.dog-container').classList.remove('dog-container--without-cat');
-    })
-  })
+      dogBreeds.classList.remove('breeds--without-cat');
+      dogContainer.classList.remove('dog-container--without-cat');
+    });
+  });
 
+  catButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    dogContainer.style.display = 'none';
+    dogBreeds.style.display = 'none';
+    catBreeds.classList.add('breeds--without-dog');
+    catContainer.classList.add('dog-container--without-dog');
 
+    const arrowButton = document.createElement('button');
+    const arrow = document.createElement('i');
+    arrow.classList.add('fas');
+    arrow.classList.add('fa-arrow-left');
+    arrowButton.classList.add('pet-button--arrow-cat');
+    arrowButton.appendChild(arrow);
+    catContainer.insertBefore(arrowButton, catButton);
 
+    arrowButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      dogContainer.style.display = 'block';
+      dogBreeds.style.display = 'flex';
+      catBreeds.classList.remove('breeds--without-dog');
+      catContainer.classList.remove('dog-container--without-dog');
+    });
+  });
 }
 
-
-
-
-
-
-export {
-  handleBigButton
-}
+export { handleBigButton };
