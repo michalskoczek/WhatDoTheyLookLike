40a07dc0cb3e835f206dceb3f1839b54dda1aef4
Michał Skoczek <mwskoczek@gmail.com>
2020-10-05 13:58:18 +0200

    Updated handle of flickity - depend on screen width

--- src/js/Flickity.js
+++ src/js/Flickity.js
@@ -1,21 +1,48 @@
-const carousels = document.querySelectorAll('.main-carousel')
+const carousels = document.querySelectorAll('.main-carousel');
+
 export default function () {
-  const flkty = new Flickity(carousels[0], {
-    cellAlign: 'left',
-    contain: true,
-    freeScroll: false,
-    wrapAround: true,
-    contain: true,
-    prevNextButtons: true,
-    pageDots: false
-  });
-  const flktySec = new Flickity(carousels[1], {
-    cellAlign: 'left',
-    contain: true,
-    freeScroll: false,
-    wrapAround: true,
-    contain: true,
-    prevNextButtons: true,
-    pageDots: false
-  });
+  const screenWidth = window.screen.width;
+  if (screenWidth < 1200) {
+    const flkty = new Flickity(carousels[0], {
+      cellAlign: 'left',
+      contain: true,
+      freeScroll: false,
+      wrapAround: true,
+      contain: true,
+      prevNextButtons: false,
+      pageDots: false
+    });
+    const flktySec = new Flickity(carousels[1], {
+      cellAlign: 'left',
+      contain: true,
+      freeScroll: false,
+      wrapAround: true,
+      contain: true,
+      prevNextButtons: false,
+      pageDots: false
+    });
+
+  } else if (screenWidth >= 1200) {
+    const flkty = new Flickity(carousels[0], {
+      cellAlign: 'left',
+      contain: true,
+      freeScroll: false,
+      wrapAround: true,
+      contain: true,
+      prevNextButtons: true,
+      pageDots: false
+    });
+    const flktySec = new Flickity(carousels[1], {
+      cellAlign: 'left',
+      contain: true,
+      freeScroll: false,
+      wrapAround: true,
+      contain: true,
+      prevNextButtons: true,
+      pageDots: false
+    });
+
+  }
+
+
 }
